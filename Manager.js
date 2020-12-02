96db5ce3a12854e68647f622236581dccd8b76e6
Peter Anderson <peterburtonanderson@gmail.com>
2020-12-02 15:08:26 -0700

    Classes set up

---
+++ Manager.js
@@ -0,0 +1,10 @@
+const Employee = require('./Employee')
+
+class Manager extends Employee {
+    constructor() {
+        this.assigned_employees = [];
+    }
+    bonus(amount) {
+        super.bonus
+    }
+}
