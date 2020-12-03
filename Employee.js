3b1a559daf08ffa6ea251e16d645344cc1ada877
Peter Anderson <peterburtonanderson@gmail.com>
2020-12-02 17:13:38 -0700

    Completed project

--- Employee.js
+++ Employee.js
@@ -1,17 +1,32 @@
 class Employee {
-    constructor(name, title, salary, boss) {
+    constructor(name, salary, title, manager = null) {
         this.name = name;
         this.title = title;
         this.salary = salary;
-        this.boss = boss;
+        this.boss = manager;
+        if (manager) manager.addEmployee(this)
     }
     bonus(amount) {
         return this.salary * amount;
     }
 
-    getSal(){
-        return this.salary;
-    }
 }
 
-module.exports.Employee = Employee;
+module.exports = { Employee };
+
+
+// class Employee {
+//     constructor(name, salary, title, manager = null) {
+//         this.name = name;
+//         this.salary = salary;
+//         this.title = title;
+//         this.manager = manager;
+//         if (manager) manager.addEmployee(this);
+//     }
+//     bonus(multiplier) {
+//         return this.salary * multiplier;
+//     }
+// }
+// module.exports = {
+//     Employee,
+// };
--- Manager.js
+++ Manager.js
@@ -4,11 +4,15 @@ const {Employee} = require('./Employee');
 
 class Manager extends Employee {
 
-    constructor(name, title, salary, boss) {
-        super(name, title, salary, boss);
+    constructor(name, salary, title, manager) {
+        super(name, salary, title, manager);
         this.assigned_employees = [];
     }
 
+    addEmployee(employee) { //"employee" here is referring to "this" which was passed in as an argument.
+        this.assigned_employees.push(employee)
+        return employee
+    }
 
     bonus(multiplier) {
         return (this.salary + this.totalSubSal()) * multiplier;
@@ -31,8 +35,14 @@ class Manager extends Employee {
 }
 
 
-// test cases:
-const the_emp = new Employee('josh', 'programmer', 80000, 'Amber');
-const the_manager = new Manager('Amber', 'head of programing', 100000, 'Robert');
+let hobbes = new Manager('Hobbes', 1000000, 'Founder', null);
+let calvin = new Manager('Calvin', 130000, 'Director', hobbes);
+let susie = new Manager('Susie', 100000, 'TA Manager', calvin);
+let lily = new Manager('Lily', 90000, 'TA', susie);
+let clifford = new Manager('Clifford', 90000, 'TA', susie);
 
-the_manager.totalSubSal();
+console.log(hobbes.bonus(0.05)); // 50000..70500
+console.log(calvin.bonus(0.05)); // 6500..20500
+console.log(susie.bonus(0.05)); // 14000--correct
+console.log(lily.bonus(0.05)); // 4500--correct
+console.log(clifford.bonus(0.05)); // 4500--correct
